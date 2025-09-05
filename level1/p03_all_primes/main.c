7391392819f93a929e4d231655da79c2025e7206
Xia FanCheng <3436097221@qq.com>
2025-09-05 22:14:21 +0800

    提交2-1000的素数打印作业

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,30 @@
-#include <printf.h>
-
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
 int main() {
-    printf("hello world!\n");
+    int sum = 0;
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+    for (int i = 2; i <=1000; i++)
+    {
+        int result = (int)sqrt(i);
+        for (int j = 2; j <= result; j++)
+        {
+            if (i % j == 0)
+            {
+                sum++;
+                break;
+            }
+        }
+        if (sum==0)
+        {
+            printf("%d ", i);
+        }
+        sum=0;
+    }
+    end = clock();
+    cpu_time_used = ((double)(end - start) / CLOCKS_PER_SEC);
+    printf("\nTime is %f\n", cpu_time_used);
     return 0;
 }
