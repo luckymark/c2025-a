1cf1d648e40be3b33b8a1878990b9ec44fe09d74
Xia FanCheng <3436097221@qq.com>
2025-09-05 20:58:07 +0800

    判断素数提交

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,31 @@
-#include <printf.h>
-
+#include <stdio.h>
+#include <math.h>
 int main() {
-    printf("hello world!\n");
+    int n;
+    int sum = 0;
+    printf("请输入一个正整数: ");
+    scanf("%d",&n);
+    if (n<2)
+    {
+        printf("%d不是素数",n);
+        return 0;
+    }
+    printf("\n");
+    int result = (int)sqrt(n);  // 计算平方根
+    for (int i = 1; i <= result; i++)
+    {
+        if (n % i == 0)
+        {
+            sum++;
+        }
+    }
+    if (sum>1)
+    {
+        printf("%d不是素数",n);
+    }
+    else
+    {
+        printf("%d是素数",n);
+    }
     return 0;
 }
